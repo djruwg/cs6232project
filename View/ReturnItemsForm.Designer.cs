1be1a9ae3956880eae752cea017cbb5d6f48b9ae
Jim Herbert <jh00648@my.westga.edu>
2024-03-23 15:53:04 -0400

    Adjusted data grid view look and feel

--- View/ReturnItemsForm.Designer.cs
+++ View/ReturnItemsForm.Designer.cs
@@ -29,33 +29,57 @@
         private void InitializeComponent()
         {
             ReturnItemsDataGridView = new DataGridView();
+            ReturnItemsTableLayoutPanel = new TableLayoutPanel();
             ((System.ComponentModel.ISupportInitialize)ReturnItemsDataGridView).BeginInit();
+            ReturnItemsTableLayoutPanel.SuspendLayout();
             SuspendLayout();
             // 
             // ReturnItemsDataGridView
             // 
+            ReturnItemsDataGridView.BackgroundColor = SystemColors.ControlLightLight;
             ReturnItemsDataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            ReturnItemsTableLayoutPanel.SetColumnSpan(ReturnItemsDataGridView, 2);
             ReturnItemsDataGridView.Dock = DockStyle.Fill;
-            ReturnItemsDataGridView.Location = new Point(0, 0);
+            ReturnItemsDataGridView.Location = new Point(3, 63);
             ReturnItemsDataGridView.Name = "ReturnItemsDataGridView";
             ReturnItemsDataGridView.RowHeadersVisible = false;
-            ReturnItemsDataGridView.Size = new Size(800, 450);
+            ReturnItemsDataGridView.Size = new Size(794, 326);
             ReturnItemsDataGridView.TabIndex = 0;
+            // 
+            // ReturnItemsTableLayoutPanel
+            // 
+            ReturnItemsTableLayoutPanel.AutoSize = true;
+            ReturnItemsTableLayoutPanel.ColumnCount = 2;
+            ReturnItemsTableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            ReturnItemsTableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            ReturnItemsTableLayoutPanel.Controls.Add(ReturnItemsDataGridView, 0, 1);
+            ReturnItemsTableLayoutPanel.Dock = DockStyle.Fill;
+            ReturnItemsTableLayoutPanel.Location = new Point(0, 0);
+            ReturnItemsTableLayoutPanel.Name = "ReturnItemsTableLayoutPanel";
+            ReturnItemsTableLayoutPanel.RowCount = 3;
+            ReturnItemsTableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 15.3846149F));
+            ReturnItemsTableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 84.61539F));
+            ReturnItemsTableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Absolute, 57F));
+            ReturnItemsTableLayoutPanel.Size = new Size(800, 450);
+            ReturnItemsTableLayoutPanel.TabIndex = 1;
             // 
             // ReturnItemsForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(ReturnItemsDataGridView);
+            Controls.Add(ReturnItemsTableLayoutPanel);
             Name = "ReturnItemsForm";
             Text = "ReturnItemsForm";
             ((System.ComponentModel.ISupportInitialize)ReturnItemsDataGridView).EndInit();
+            ReturnItemsTableLayoutPanel.ResumeLayout(false);
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private DataGridView ReturnItemsDataGridView;
+        private TableLayoutPanel ReturnItemsTableLayoutPanel;
     }
 }
